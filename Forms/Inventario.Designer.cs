9fc8dbb4e0e26f442b79934bd43d1b4bd84eccc9
Martin Moschettoni <moschettonimartin@hotmail.com>
2020-12-15 22:58:46 -0300

    Inventario: Vista basica

---
+++ Forms/Inventario.Designer.cs
@@ -0,0 +1,232 @@
+﻿
+namespace Todo_en_uno.Forms
+{
+    partial class Inventario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.BtnEliminar = new System.Windows.Forms.Button();
+            this.BtnEditar = new System.Windows.Forms.Button();
+            this.BtnAgregar = new System.Windows.Forms.Button();
+            this.NombreProducto = new System.Windows.Forms.Label();
+            this.CheckStock = new System.Windows.Forms.CheckBox();
+            this.TxtCodigo = new System.Windows.Forms.TextBox();
+            this.TxtNombre = new System.Windows.Forms.TextBox();
+            this.CodigoProducto = new System.Windows.Forms.Label();
+            this.BtnBuscar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 86;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.IsSplitterFixed = true;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.BtnEliminar);
+            this.splitContainer2.Panel1.Controls.Add(this.BtnEditar);
+            this.splitContainer2.Panel1.Controls.Add(this.BtnAgregar);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.BtnBuscar);
+            this.splitContainer2.Panel2.Controls.Add(this.CodigoProducto);
+            this.splitContainer2.Panel2.Controls.Add(this.NombreProducto);
+            this.splitContainer2.Panel2.Controls.Add(this.CheckStock);
+            this.splitContainer2.Panel2.Controls.Add(this.TxtCodigo);
+            this.splitContainer2.Panel2.Controls.Add(this.TxtNombre);
+            this.splitContainer2.Size = new System.Drawing.Size(800, 86);
+            this.splitContainer2.SplitterDistance = 396;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // BtnEliminar
+            // 
+            this.BtnEliminar.AutoEllipsis = true;
+            this.BtnEliminar.Cursor = System.Windows.Forms.Cursors.Default;
+            this.BtnEliminar.Location = new System.Drawing.Point(262, 12);
+            this.BtnEliminar.Name = "BtnEliminar";
+            this.BtnEliminar.Size = new System.Drawing.Size(119, 57);
+            this.BtnEliminar.TabIndex = 2;
+            this.BtnEliminar.Text = "Eliminar Producto";
+            this.BtnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // BtnEditar
+            // 
+            this.BtnEditar.AutoEllipsis = true;
+            this.BtnEditar.Cursor = System.Windows.Forms.Cursors.Default;
+            this.BtnEditar.Location = new System.Drawing.Point(137, 12);
+            this.BtnEditar.Name = "BtnEditar";
+            this.BtnEditar.Size = new System.Drawing.Size(119, 57);
+            this.BtnEditar.TabIndex = 1;
+            this.BtnEditar.Text = "Editar Producto";
+            this.BtnEditar.UseVisualStyleBackColor = true;
+            // 
+            // BtnAgregar
+            // 
+            this.BtnAgregar.AutoEllipsis = true;
+            this.BtnAgregar.Cursor = System.Windows.Forms.Cursors.Default;
+            this.BtnAgregar.Location = new System.Drawing.Point(12, 12);
+            this.BtnAgregar.Name = "BtnAgregar";
+            this.BtnAgregar.Size = new System.Drawing.Size(119, 57);
+            this.BtnAgregar.TabIndex = 0;
+            this.BtnAgregar.Text = "Agregar Producto";
+            this.BtnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // NombreProducto
+            // 
+            this.NombreProducto.AutoSize = true;
+            this.NombreProducto.Location = new System.Drawing.Point(14, 15);
+            this.NombreProducto.Name = "NombreProducto";
+            this.NombreProducto.Size = new System.Drawing.Size(104, 13);
+            this.NombreProducto.TabIndex = 3;
+            this.NombreProducto.Text = "Nombre de producto";
+            // 
+            // CheckStock
+            // 
+            this.CheckStock.AutoSize = true;
+            this.CheckStock.Location = new System.Drawing.Point(88, 57);
+            this.CheckStock.Name = "CheckStock";
+            this.CheckStock.Size = new System.Drawing.Size(82, 17);
+            this.CheckStock.TabIndex = 2;
+            this.CheckStock.Text = "Poco Stock";
+            this.CheckStock.UseVisualStyleBackColor = true;
+            // 
+            // TxtCodigo
+            // 
+            this.TxtCodigo.Location = new System.Drawing.Point(136, 31);
+            this.TxtCodigo.Name = "TxtCodigo";
+            this.TxtCodigo.Size = new System.Drawing.Size(100, 20);
+            this.TxtCodigo.TabIndex = 1;
+            // 
+            // TxtNombre
+            // 
+            this.TxtNombre.Location = new System.Drawing.Point(17, 31);
+            this.TxtNombre.Name = "TxtNombre";
+            this.TxtNombre.Size = new System.Drawing.Size(100, 20);
+            this.TxtNombre.TabIndex = 0;
+            // 
+            // CodigoProducto
+            // 
+            this.CodigoProducto.AutoSize = true;
+            this.CodigoProducto.Location = new System.Drawing.Point(136, 15);
+            this.CodigoProducto.Name = "CodigoProducto";
+            this.CodigoProducto.Size = new System.Drawing.Size(100, 13);
+            this.CodigoProducto.TabIndex = 4;
+            this.CodigoProducto.Text = "Codigo de producto";
+            // 
+            // BtnBuscar
+            // 
+            this.BtnBuscar.Location = new System.Drawing.Point(272, 19);
+            this.BtnBuscar.Name = "BtnBuscar";
+            this.BtnBuscar.Size = new System.Drawing.Size(116, 42);
+            this.BtnBuscar.TabIndex = 5;
+            this.BtnBuscar.Text = "Buscar";
+            this.BtnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(800, 360);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Inventario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Inventario";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.Button BtnEliminar;
+        private System.Windows.Forms.Button BtnEditar;
+        private System.Windows.Forms.Button BtnAgregar;
+        private System.Windows.Forms.Label NombreProducto;
+        private System.Windows.Forms.CheckBox CheckStock;
+        private System.Windows.Forms.TextBox TxtCodigo;
+        private System.Windows.Forms.TextBox TxtNombre;
+        private System.Windows.Forms.Button BtnBuscar;
+        private System.Windows.Forms.Label CodigoProducto;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
