12b4f72bb0ff567b34a0ab95004f8cd8109759f1
mili.aylen.j@gmail.com <Lorylan@DESKTOP-1DI1S74>
2020-12-17 22:35:57 -0300

    Agrega clase venta

---
+++ Clases/Venta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo_en_uno.Clases
+{
+    public class Venta
+    {
+        public List<Productos> List_productos{ get; set; }
+        public Boolean Cerrada;
+
+        public Venta() {
+            Cerrada = false;
+            List_productos = new List<Productos>();
+        }
+
+        public void agregar_producto(Producto p) { 
+            //implementar el agregar venta
+        }
+    }
+
+}
