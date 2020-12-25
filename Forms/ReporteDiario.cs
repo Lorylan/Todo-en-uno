b025688564fded0a69a8d03abb688266f99d2c24
mili.aylen.j@gmail.com <Lorylan@DESKTOP-1DI1S74>
2020-12-25 14:21:03 -0300

    Implementa la parte grafica de reporte diario

---
+++ Forms/ReporteDiario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Todo_en_uno.Forms
+{
+    public partial class ReporteDiario : Form
+    {
+        public ReporteDiario()
+        {
+            InitializeComponent();
+        }
+    }
+}
